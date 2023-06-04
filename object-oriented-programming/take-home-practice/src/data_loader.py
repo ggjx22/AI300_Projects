de13fa26d528d7472fa90378e4ae302071c8b1f8
ggjx22 <navlag18011@gmail.com>
2023-06-04 22:18:44 +0800

    move methods get_dataset_shape and check_any_missing to class ExploreDataAnalysis

--- object-oriented-programming/take-home-practice/src/data_loader.py
+++ object-oriented-programming/take-home-practice/src/data_loader.py
@@ -15,14 +15,6 @@ class DataLoader:
         print(f'Data has been sucessfully loaded from {self.path_to_data}')
         return data
     
-    # method that checks shape of the data set
-    def get_dataset_shape(self, data):
-        return data.shape
-    
-    # method that checks for missing data in data set
-    def check_any_missing(self, data):
-        return data.isnull().sum()
-    
     # method that splits the data into predictors (features) being X and target being y
     def split_data(self, data):
         X = data.drop('Loan_Status', axis=1)
@@ -30,29 +22,22 @@ class DataLoader:
         return X, y
 
 
-# Uncomment test cases below once class is built
+# # Uncomment test cases below once class is built
 
-### test development ###
-path = 'object-oriented-programming/take-home-practice/data/loan_prediction.csv' # relative path to csv file
+# ### test development ###
+# path = 'object-oriented-programming/take-home-practice/data/loan_prediction.csv' # relative path to csv file
 
-# checks path of data
-data = DataLoader(path)
-assert data.path_to_data == path
+# # checks path of data
+# data = DataLoader(path)
+# assert data.path_to_data == path
 
-# checks if pandas dataframe has been loaded
-df = data.load_data()
-assert isinstance(df, pd.DataFrame)
+# # checks if pandas dataframe has been loaded
+# df = data.load_data()
+# assert isinstance(df, pd.DataFrame)
 
-# check any missing data
-print(data.check_any_missing(df))
+# # check if dataset is split succesfully
+# X, y = data.split_data(df)
+# print(X)
+# print(y)
 
-# checks size of dataframe
-print(data.get_dataset_shape(df))
-assert data.get_dataset_shape(df) == (614,13)
-
-# check if dataset is split succesfully
-X, y = data.split_data(df)
-print(X)
-print(y)
-
-print('All test passed.')
+# print('All test passed.')
